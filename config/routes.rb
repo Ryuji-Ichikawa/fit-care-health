8fefc52d7754dc0475ee2c94f3ddb8802493631f
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-05-31 16:11:50 +0900

    同時ゲストユーザーの編集/削除不可

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords:     'users/passwords'
+  }
   root to: 'health#index'
-  
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
