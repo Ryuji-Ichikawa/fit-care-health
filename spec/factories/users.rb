44743aeff7fa05a8baf331faf3b3a564b8866f67
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-01 00:00:06 +0900

    test_factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { '1a' + Faker::Internet.password(min_length: 4) }
+    password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     after(:build) do |user|
       user.image.attach(io: File.open('public/image/test-image.jpg'), filename: 'test-image.jpg')
