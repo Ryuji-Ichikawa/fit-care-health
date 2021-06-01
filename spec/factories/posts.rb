4ccbfe35dddfff8686fb02641e40c7f6dc84ebc1
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-01 18:11:48 +0900

    factories_editing

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :post do
+    title       {Faker::Name.initials(number: 6)}
+    catch_copy  {Faker::Lorem.sentence}
+    concept     {Faker::Lorem.sentence}
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/image/test-image.jpg'), filename: 'test-image.jpg')
+    end
   end
 end
