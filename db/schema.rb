2af6dd65f9fe4823e39ea19f7005959165deade6
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-01 12:26:34 +0900

    DB_create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_024646) do
+ActiveRecord::Schema.define(version: 2021_06_01_022502) do
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id"
+    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "statuses", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -35,5 +46,6 @@ ActiveRecord::Schema.define(version: 2021_05_31_024646) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "statuses", "users"
 end
