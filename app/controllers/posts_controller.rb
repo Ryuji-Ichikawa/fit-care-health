a37f038bf77328366e74548966bfd730d583bfc2
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-01 17:24:14 +0900

    delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
   def index
     @posts = Post.all
@@ -33,6 +33,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
