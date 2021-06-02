2759356153c5bd01ca128cfa11ecc4524a5aa95c
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-02 12:44:51 +0900

    first_test

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,8 +3,9 @@ FactoryBot.define do
     title       {Faker::Name.initials(number: 6)}
     catch_copy  {Faker::Lorem.sentence}
     concept     {Faker::Lorem.sentence}
-    after(:build) do |user|
-      user.image.attach(io: File.open('public/image/test-image.jpg'), filename: 'test-image.jpg')
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/image/test-image.jpg'), filename: 'test-image.jpg')
     end
+    association :user 
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '投稿の保存' do
+    context '投稿ができる場合' do
+      it 'すべての項目があれば投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿ができない場合' do
+      it 'タイトルが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end     
+      
+    end
+  end
 end
