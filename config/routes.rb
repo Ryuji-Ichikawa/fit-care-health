ec8eb482ba4c3d9f4c0662be9e84b3d4ee7cb272
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-03 14:18:54 +0900

    resources_config

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: :create
   end
+  resources :users, only: :show
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
