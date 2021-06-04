da1e136ee319ec433521167fc3539804ab4e208c
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-04 15:37:13 +0900

    routes_editing

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: :create
     collection do
-      get 'search'
+      get 'list'
     end
   end
   resources :users, only: :show
