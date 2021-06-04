ee06cc5b37f6560152d21c0f51faebe4573d4c62
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-04 18:07:15 +0900

    likes_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
       user.profile = 'No Profile'
     end
   end
-  def already_liked?(post)
-    self.likes.exists?(post_id: post.id)
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
   end
   has_many :posts
-  has_many :comments
-  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes,    dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_one_attached :image
 
