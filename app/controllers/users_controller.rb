e81be27c525c7c491aa803be2855725128f46bf8
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-15 21:52:22 +0900

    controller_editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts
   end
+
+  def edit
+    redirect_to root_path unless @post.user_id == current_user.id
+  end
+
+  def update
+    if @user.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:image, :nickname, :email, :profile)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
+
