dd1f9d7cc422453b499e057480e1b660de4d974b
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-21 22:41:03 +0900

    model_editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   def self.guest
@@ -19,8 +17,25 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :post
   has_one_attached :image
 
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+  has_many :following, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
   with_options presence: true do
     validates :nickname
     validates :profile
+  end
+  
+  def following?(user)
+    following_relationships.find_by(following_id: user.id)
+  end
+
+  def follow(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow(user)
+    following_relationships.find_by(following_id: user.id).destroy
   end
 end
