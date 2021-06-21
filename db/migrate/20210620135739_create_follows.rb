f0f1664509b0f2b116ecc193bd550686b00ab73d
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-21 22:38:28 +0900

    database_created

--- db/migrate/20210620135739_create_follows.rb
+++ db/migrate/20210620135739_create_follows.rb
@@ -3,6 +3,8 @@ class CreateFollows < ActiveRecord::Migration[6.1]
     create_table :follows do |t|
       t.integer :follower_id
       t.integer :following_id
+      
+      t.index [:follower_id, :following_id], unique: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_072618) do
+ActiveRecord::Schema.define(version: 2021_06_20_135739) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 2021_06_04_072618) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "follows", charset: "utf8", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "following_id"], name: "index_follows_on_follower_id_and_following_id", unique: true
   end
 
   create_table "likes", charset: "utf8", force: :cascade do |t|
