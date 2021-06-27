67110adbdf24da260ad45b5f399b1f6bd50f0d3a
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-28 05:52:28 +0900

    controller_setting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show, :search]
   def index
+    @tag_list = Tag.all 
     @posts = Post.all
+    @post = current_user.posts.new
   end
 
   def new
@@ -11,8 +13,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
+    tag_list = params[:post][:tag_name].split(nil)
     if @post.save
+      @post.save_tag(tag_list) 
       redirect_to root_path
     else
       render :new
@@ -20,6 +24,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post_tags = @post.tags 
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
     @like = Like.new
@@ -59,5 +64,20 @@ class PostsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+
+    old_tags.each do |old|
+      self.post_tags.delete PostTag.find_by(tag_name: old)
+    end
+
+    new_tags.each do |new|
+      new_post_tag = PostTag.find_or_create_by(tag_name: new)
+      self.post_tags << new_post_tag
+    end
   end
 end
