c7b484780e199c18fd860ee05ffdf1deea88a2dd
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-06-27 17:29:06 +0900

    database_create

--- db/migrate/20210627075644_create_tag_maps.rb
+++ db/migrate/20210627075644_create_tag_maps.rb
@@ -1,6 +1,8 @@
 class CreateTagMaps < ActiveRecord::Migration[6.1]
   def change
     create_table :tag_maps do |t|
+      t.references :post, foreign_key: true
+      t.references :tag, foreign_key: true
 
       t.timestamps
     end
