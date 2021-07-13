68db58ffcead0e90e12fafb724ffd2fb7573e877
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-07-14 03:55:28 +0900

    tag_spec_association

--- spec/models/tag_map_spec.rb
+++ spec/models/tag_map_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe TagMap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーション' do
+    let(:association) do
+       described_class.reflect_on_association(target)
+    end
+
+    context 'Postモデルとの関係' do
+      let(:target) { :post }
+      it '多:1' do
+        expect(association.macro).to eq :belongs_to
+      end
+      it '結合するモデルのクラス名：Post' do
+        expect(association.class_name).to eq 'Post'
+      end
+    end
+
+    context 'Tagモデルとの関係' do
+      let(:target) { :tag }
+      it '多:1' do
+        expect(association.macro).to eq :belongs_to
+      end
+      it '結合するモデルのクラス名：Tag' do
+        expect(association.class_name).to eq 'Tag'
+      end
+    end
+  end
 end
