5ab06291f2f126a5904223d156d9ade81e0a6775
Ryuji-Ichikawa <r_rex12@yahoo.co.jp>
2021-07-14 02:53:31 +0900

    comment_add_association

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -29,4 +29,30 @@ RSpec.describe Comment, type: :model do
       end
     end
   end
+
+  describe 'アソシエーション' do
+    let(:association) do
+       described_class.reflect_on_association(target)
+    end
+
+    context 'Postモデルとの関係' do
+      let(:target) { :post }
+      it '多:1' do
+        expect(association.macro).to eq :belongs_to
+      end
+      it '結合するモデルのクラス名：Post' do
+        expect(association.class_name).to eq 'Post'
+      end
+    end
+
+    context 'Userモデルとの関係' do
+      let(:target) { :user }
+      it '多:1' do
+        expect(association.macro).to eq :belongs_to
+      end
+      it '結合するモデルのクラス名：User' do
+        expect(association.class_name).to eq 'User'
+      end
+    end
+  end
 end
